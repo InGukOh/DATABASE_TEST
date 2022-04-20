25ce407ed22f56612181b7f280eedad00dd81551
Trident12kilo <dhdlsrnr1231@naver.com>
2022-04-21 02:05:54 +0900

    DB 행 갯수 및 내용 호출 이해 / 04/21 /01

--- app/src/main/java/com/example/database_test/MainActivity.java
+++ app/src/main/java/com/example/database_test/MainActivity.java
@@ -12,9 +12,12 @@ import android.view.ViewGroup;
 import android.view.ViewManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     Cursor cursor;
 
-    TextView num , edit_schedule, edit_timer;
+    TextView DB_DATA;
     EditText setSchedule, setTimer, listSchedule, listTimer;
     Button btnInsert, btnSelect, btnReset, btnCreate, listDelThis;
 
@@ -48,9 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
         setSchedule = findViewById(R.id.add_schedule);
         setTimer = findViewById(R.id.add_timer);
-        num = findViewById(R.id.num);
-        edit_schedule = findViewById(R.id.edtNameResult);
-        edit_timer = findViewById(R.id.edtNumberResult);
         btnInsert = findViewById(R.id.btnInsert);
         btnSelect = findViewById(R.id.btnSelect);
         btnReset = findViewById(R.id.btnReset);
@@ -66,23 +66,14 @@ public class MainActivity extends AppCompatActivity {
         dbHelper = new DBHelper(this);
         sqlDB = dbHelper.getReadableDatabase();
 
-
-        test = 0;
-
         cursor = sqlDB.rawQuery("SELECT * FROM groupTBL;", null);
-
-        DBView();
-
-        cursor = sqlDB.rawQuery("SELECT * FROM groupTBL;",null);
 
         test = cursor.getCount();
         timer_Create(test);
+
         Toast.makeText(getApplicationContext(),"테스트 "+test +" ",Toast.LENGTH_SHORT).show();
         cursor.close();
         sqlDB.close();
-
-
-        //내 DB클래스 객체생성
 
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,30 +107,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onClick_Select(View v) {
-        //커서를 선언하고 모든 테이블을 조회한 후 커서에 대입한다.
-        //이 과정이 조금 이해가 어려운데, 이 커서 객체가 포인터 역할을 한다고 이해하면 된다.
         sqlDB = dbHelper.getReadableDatabase();
         Cursor cursor;
         cursor = sqlDB.rawQuery("SELECT * FROM groupTBL;", null);
-
-
-        //커서가 움직이면서  현재 커서의 열 번호 데이터값을 반환해서 문자열 변수에 계속 누적한다.
-        //0은 0번째열(그룹이름) , 1은 1번째열(인원)이 된다.
-        while(cursor.moveToNext()){
-            strNum += cursor.getString(0)+ "\r\n";
-            strSchedule += cursor.getString(1) + "\r\n";
-            strTimer += cursor.getString(2) + "\r\n";
-
-
-        //이름 출력해주기
-        num.setText(strNum);
-        edit_schedule.setText(strSchedule);
-        edit_timer.setText(strTimer);
-        }
         cursor.close();
         sqlDB.close();
     }
     public void onClick_Insert(View view) {
+        add_Timer_Layout.removeAllViewsInLayout();
         sqlDB = dbHelper.getWritableDatabase();
         sqlDB.execSQL("INSERT INTO groupTBL(gName,gNumber) VALUES (" +
                 "'" + setSchedule.getText().toString() + "' ,"
@@ -149,15 +124,12 @@ public class MainActivity extends AppCompatActivity {
         timer_Create(test);
         sqlDB.close();
         Toast.makeText(getApplicationContext(), "입력됨", Toast.LENGTH_SHORT).show();
-        overridePendingTransition(0, 0);//인텐트 효과 없애기
-        Intent intent = getIntent(); //인텐트
-        startActivity(intent);
     }
     public void onClick_Drop(View view) {
         sqlDB = dbHelper.getWritableDatabase();
-        add_Timer_Layout.removeAllViewsInLayout();
         sqlDB.execSQL("Drop table IF EXISTS groupTBL");
         onClick_Create(view);
+        add_Timer_Layout.removeAllViewsInLayout();
         sqlDB.close();
     }
     public void onClick_Create(View view) {
@@ -167,29 +139,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void timer_Create(int callCount){
-        add_Timer_Layout.removeAllViewsInLayout();
         int start = 0;
+        sqlDB = dbHelper.getWritableDatabase();
+
         while (start < callCount){
-            LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            EditText et = new EditText(getApplicationContext());
-            et.setLayoutParams(p);
-            et.setText("editText" + start + "번");
-            et.setId(start);
-            add_Timer_Layout.addView(et);
+            LinearLayout p2 = new LinearLayout(this);
+            p2.setOrientation(LinearLayout.HORIZONTAL);
+            p2.setGravity(Gravity.CENTER);
+            LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(400, ViewGroup.LayoutParams.WRAP_CONTENT,LinearLayout.HORIZONTAL);
+
+            TextView Num = new TextView(getApplicationContext());
+            EditText Schedule = new EditText(getApplicationContext());
+            EditText Timer = new EditText(getApplicationContext());
+
+            Num.setLayoutParams(p);
+            Schedule.setLayoutParams(p);
+            Timer.setLayoutParams(p);
+            int starter = 1;
+            while(starter <= callCount){
+                cursor = sqlDB.rawQuery("SELECT * FROM groupTBL where num = "+ starter ,null);
+                cursor.moveToFirst();
+                String number = cursor.getString(0);
+                String SC = cursor.getString(1);
+                Num.setId(start);
+                Num.setText(number);
+                Schedule.setText(SC);
+                Timer.setText("editText" + start + "번");
+                Schedule.setId(start);
+                Timer.setId(start);
+                starter++;
+            }
+            p2.addView(Num);
+            p2.addView(Schedule);
+            p2.addView(Timer);
+            add_Timer_Layout.addView(p2);
+
             start++;
         }
-    }
-    public  void DBView(){
-       /* DBView.removeAllViewsInLayout();*/
-        while(cursor.moveToNext()){
-            strNum += cursor.getString(0)+ "\r\n";
-            strSchedule += cursor.getString(1) + "\r\n";
-            strTimer += cursor.getString(2) + "\r\n";
-        }
-
-        //이름 출력해주기
-        num.setText(strNum);
-        edit_schedule.setText(strSchedule);
-        edit_timer.setText(strTimer);
     }
 }
